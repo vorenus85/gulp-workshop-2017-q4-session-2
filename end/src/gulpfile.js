b54df5ffcce3fe587f48ee7cba7e81202258b2d0
Janos Perge <janos.perge@innonic.com>
2017-12-03 16:47:52 +0100

    create basic sass task

--- end/src/gulpfile.js
+++ end/src/gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// static variables
+var srcScss = 'scss/style.scss';
+var distCss = '../web/css';
+
+gulp.task('sass', function(){
+    return gulp.src(srcScss)
+    .pipe(sass({})) // Converts Sass to Css with gulp sass
+    .pipe(gulp.dest(distCss));
+});
