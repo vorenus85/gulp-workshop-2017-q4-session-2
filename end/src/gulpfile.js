829e9e0688d13566471d539db3e4b89c563f8925
Janos Perge <janos.perge@innonic.com>
2017-12-03 17:12:45 +0100

    add gulp-plumber

--- end/src/gulpfile.js
+++ end/src/gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 // static variables
 var srcScss = 'scss/style.scss';
 var distCss = '../web/css';
 
 gulp.task('sass', function(){
+    console.log('starting sass task');
     return gulp.src(srcScss)
+    .pipe(plumber(function (err){
+      console.log('Sass task error');
+      console.log(err);
+      this.emit('end');
+    }))
     .pipe(sass({})) // Converts Sass to Css with gulp sass
     .pipe(gulp.dest(distCss));
 });
